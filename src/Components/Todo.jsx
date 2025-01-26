6a5928a5b292eb9838e04fc18f5af695e701402e
mahal teja <mahaltejapilla08@gmail.com>
2025-01-26 18:39:13 +0530

    Updated Add Task functionality

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -8,6 +8,7 @@ const Todo = () => {
   const [isOpen, setOpen] = useState(true);
     const [taskStatus, setTaskstatus] = useState(false);
     const [taskcategory, setTaskcategory] = useState(false);
+    const [taskform,setForm] =useState(false)
 
     const [formData,setData] =useState({
     task_name:"",
@@ -31,6 +32,7 @@ const Todo = () => {
     console.log(Task_List)
     setTaskstatus(false)
     setTaskcategory(false)
+    setForm(false)
     }
 
   return (
@@ -48,64 +50,65 @@ const Todo = () => {
       </div>
       {isOpen && (
         <div className="todo-sec-bottom">
-          <div className="add-task-sec">
+          <div className="add-task-sec" onClick={()=> setForm(!taskform)}>
             <img src={IMAGES.Plus_Icon} alt="Plus Icon" />
             <p className="add-task-text">Add Task</p>
           </div>
-
-        <form className="form-container" onSubmit={handleSubmit}>
-        <div className="add-form">
-            <input type="text" id="input" placeholder="Task Title" className="input-bar" name="task_name" value={formData.task_name} onChange={handleChange}  required />
-            <div id="popup-row">
-                <span className="span-css">
-                    <img src={IMAGES.Calender_Icon} alt="" />
-                    <p className="cal-text">Add Date</p>
-                </span>
-                <input type="date" id="input" name="date" className="input-cal" value={formData.date} onChange={handleChange} required />
+          { taskform &&
+            <form className="form-container" onSubmit={handleSubmit}>
+            <div className="add-form">
+                <input type="text" id="input" placeholder="Task Title" className="input-bar" name="task_name" value={formData.task_name} onChange={handleChange}  required />
+                <div id="popup-row">
+                    <span className="span-css">
+                        <img src={IMAGES.Calender_Icon} alt="" />
+                        <p className="cal-text">Add Date</p>
+                    </span>
+                    <input type="date" id="input" name="date" className="input-cal" value={formData.date} onChange={handleChange} required />
+                </div>
+                <div id="task-status-dropdown">
+                <img src={IMAGES.Plus_Icon}  alt="Plus Icon"  onClick={() => setTaskstatus(!taskStatus)} />
+                
+                    <div id={taskStatus ? "status-popup":"status-popup-close"} required>
+                    <div id="popup-row">
+                        <input type="radio" id="todo" name="status" value="TO-DO" onChange={handleChange}  required />
+                        <label for="todo">TO-DO</label>
+                    </div>
+                    <div id="popup-row">
+                        <input type="radio" id="inprogress" name="status" value="In-Progress" onChange={handleChange}  required />
+                        <label for="inprogress">In-Progress</label>
+                    </div>
+                    <div id="popup-row">
+                        <input type="radio" id="completed" name="status" value="Completed"   onChange={handleChange}  required/>
+                        <label for="completed">Completed</label>
+                    </div>
+                    </div>
+                
+                </div>
+                <div id="task-status-dropdown">
+                <img src={IMAGES.Plus_Icon} alt="Plus Icon" onClick={() => setTaskcategory(!taskcategory)}/>
+              
+                    <div id={taskcategory ? "status-popup":"status-popup-close"} required>
+                    <div id="popup-row">
+                        <input  type="radio" id="work"  name="category" value="Work" onChange={handleChange} required/>
+                        <label for="work">Work</label>
+                    </div>
+                    <div id="popup-row">
+                        <input type="radio" id="personal" name="category" value="Personal" onChange={handleChange} required/>
+                        <label for="personal">Personal</label>
+                    </div>
+                    </div>
+          
+                </div>
             </div>
-            <div id="task-status-dropdown">
-            <img src={IMAGES.Plus_Icon}  alt="Plus Icon"  onClick={() => setTaskstatus(!taskStatus)} />
-            
-                <div id={taskStatus ? "status-popup":"status-popup-close"} required>
-                <div id="popup-row">
-                    <input type="radio" id="todo" name="status" value="TO-DO" onChange={handleChange}  required />
-                    <label for="todo">TO-DO</label>
-                </div>
-                <div id="popup-row">
-                    <input type="radio" id="inprogress" name="status" value="In-Progress" onChange={handleChange}  required />
-                    <label for="inprogress">In-Progress</label>
-                </div>
-                <div id="popup-row">
-                    <input type="radio" id="completed" name="status" value="Completed"   onChange={handleChange}  required/>
-                    <label for="completed">Completed</label>
-                </div>
-                </div>
-            
+            <div className="form-btn-cont">
+                <button className="add-btn">
+                ADD
+                <img src={IMAGES.Enter_Icon} alt="" />
+                </button>
+                <button className="cancel-btn" onClick={()=>{setForm(false)}}>CANCEL</button>
             </div>
-            <div id="task-status-dropdown">
-            <img src={IMAGES.Plus_Icon} alt="Plus Icon" onClick={() => setTaskcategory(!taskcategory)}/>
-           
-                <div id={taskcategory ? "status-popup":"status-popup-close"} required>
-                <div id="popup-row">
-                    <input  type="radio" id="work"  name="category" value="Work" onChange={handleChange} required/>
-                    <label for="work">Work</label>
-                </div>
-                <div id="popup-row">
-                    <input type="radio" id="personal" name="category" value="Personal" onChange={handleChange} required/>
-                    <label for="personal">Personal</label>
-                </div>
-                </div>
-       
-            </div>
-        </div>
-        <div className="form-btn-cont">
-            <button className="add-btn">
-            ADD
-            <img src={IMAGES.Enter_Icon} alt="" />
-            </button>
-            {/* <button className="cancel-btn">CANCEL</button> */}
-        </div>
-        </form>
+            </form>
+        }
 
           {Task_List.length > 0 ? (
             Task_List.filter((item)=>item.status==="TO-DO").map((item) => {
